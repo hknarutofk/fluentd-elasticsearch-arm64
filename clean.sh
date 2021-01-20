9c4bfa2a7b6a43166aa89fe97a0c3705a1b97524
toyangdon <toyangdon@163.com>
2021-01-20 10:18:44 +0800

    完善清除，发布脚本

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ls *.yaml | xargs -i kubectl delete -f {}
+kubectl get pvc -n kube-system | grep elasticsearch-logging | awk '{print $1}' | xargs -i kubectl delete pvc -n kube-system {}
+
